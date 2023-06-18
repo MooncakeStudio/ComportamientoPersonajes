333c8521fc672ffc652674d83b26c1f214fbdc2f
Pablo4lf <pablo.alf2001@gmail.com>
2023-06-18 03:43:31 +0200

    cura con límite

--- Assets/Script/Personaje/MagoController.cs
+++ Assets/Script/Personaje/MagoController.cs
@@ -10,7 +10,7 @@ public class MagoController : PersonajeController
 
     protected override void Awake()
     {
-        this.personaje = new Mago(VidaPoner, 10, "",5);
+        this.personaje = new Mago(VidaPoner, 10, "", 5);
         this.personaje.SetVida(VidaPoner);
 
         GetComponent<BTMago>().GetBT().Active = false;
@@ -33,7 +33,10 @@ public class MagoController : PersonajeController
         Debug.Log(aliado.gameObject.name + " está siendo curado");
         Debug.Log("Vida de :" + aliado.gameObject.name + " es: " + aliado.GetPersonaje().GetVida());
         var mago = personaje as Mago;
+
         aliado.GetPersonaje().SetVida(aliado.GetPersonaje().GetVida() + mago.GetCuracion());
+        Mathf.Clamp(aliado.GetPersonaje().GetVida(), 0, 100);
         Debug.Log("Vida de :" + aliado.gameObject.name + " tras curar es: " + aliado.GetPersonaje().GetVida());
+
     }
 }
